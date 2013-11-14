940c696d89360ce34b6ddbbf29b444a411cac404
Dino Kraljevi† <dino.kraljevic@gmail.com>
2013-11-14 16:16:27 +0100

    added validation tests for presence and uniqueness of user model propertie

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,11 @@ class User < ActiveRecord::Base
   attr_accessible :first_name, :last_name, :profile_name, :email, :password, :password_confirmation, :remember_me
   has_many :statuses
 
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :profile_name, presence: true,
+                           uniqueness: true
+
   def full_name
           first_name + " " + last_name
   end        
--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,7 +1,36 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  test "a user should enter fn" do 
+  	user = User.new
+  	assert !user.save
+  	assert !user.errors[:first_name].empty?
+  end
+
+  test "a user should enter ln" do 
+  	user = User.new
+  	assert !user.save
+  	assert !user.errors[:last_name].empty?
+  end
+
+   test "a user should enter pn" do 
+  	user = User.new
+  	assert !user.save
+  	assert !user.errors[:profile_name].empty?
+  end
+
+  test "profile name should be unique" do
+  	user = User.new
+  	user.profile_name = "dinotembo"
+  	user.first_name = "dino"
+  	user.last_name = "kraljevic"
+  	user.email = "dino.kraljevic@gmail.com"
+  	user.password = "password"
+  	user.password_confirmation = "password"
+  	assert !user.save
+  	assert !user.errors[:profile_name].empty?
+  end
+
+
 end
